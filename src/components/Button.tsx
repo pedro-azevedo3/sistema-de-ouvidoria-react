3b2ec68ec4064b76537c47ac5693f07cb641cd3a
Dev Pedro Azevedo <pedroazevedoteixeira7@gmail.com>
2024-03-10 01:32:20 -0300

    Animated the buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,35 @@
-import praise from '../assets/praise.png'
-import denounce from '../assets/denounce.png'
-import simplify from '../assets/simplify.png'
-import support from '../assets/support.png'
-import complaint from '../assets/complaint.png'
+import { useState } from 'react';
+import praise from '../assets/praise.png';
+import denounce from '../assets/denounce.png';
+import simplify from '../assets/simplify.png';
+import support from '../assets/support.png';
+import complaint from '../assets/complaint.png';
 
-const Buttons = () => {
+const AnimatedImage: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
   return (
-    <div  className='ml-10 mr-10 grid gap-4 grid-cols-3 grid-rows-3'>
-      <div className="artboard artboard-horizontal phone-1 cursor-pointer"> 
-        <img src={denounce} className="max-w-full lg:max-w-96 rounded-lg lg:mr-16" />
-      </div>
-      <div className="artboard artboard-horizontal phone-1 cursor-pointer"> 
-        <img src={praise} className="max-w-full lg:max-w-96 rounded-lg lg:mr-16" />
-      </div>
-      <div className="artboard artboard-horizontal phone-1 cursor-pointer"> 
-        <img src={complaint} className="max-w-full lg:max-w-96 rounded-lg lg:mr-16" />
-      </div>
-      <div className="artboard artboard-horizontal phone-1 cursor-pointer"> 
-        <img src={simplify} className="max-w-full lg:max-w-96 rounded-lg lg:mr-16" />
-      </div>
-      <div className="artboard artboard-horizontal phone-1 cursor-pointer"> 
-        <img src={support} className="max-w-full lg:max-w-96 rounded-lg lg:mr-16" />
-      </div>
+    <div 
+      className="relative overflow-hidden transition-transform duration-300 ease-in-out transform-gpu hover:scale-105 hover:z-10"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={src} alt={alt} className="max-w-full rounded-lg" />
+      {isHovered && <div className="backdrop-filter absolute inset-0 pointer-events-none"></div>}
     </div>
-  )
+  );
+};
+
+const Buttons: React.FC = () => {
+  return (
+    <div className='mx-10 my-4 grid gap-4 grid-cols-2 lg:grid-cols-4'>
+      <AnimatedImage src={denounce} alt="Denounce" />
+      <AnimatedImage src={praise} alt="Praise" />
+      <AnimatedImage src={complaint} alt="Complaint" />
+      <AnimatedImage src={simplify} alt="Simplify" />
+      <AnimatedImage src={support} alt="Support" />
+    </div>
+  );
 }
 
-export default Buttons
+export default Buttons;
